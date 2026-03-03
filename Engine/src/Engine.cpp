2decec3d69cae8362b5f93aeaf8c834ce8be32f4
RaniNagels <rani.nagels@outlook.be>
2026-03-03 15:47:23 +0100

    change name of Minigin to Engine for emscripten specific code

--- Engine/src/Engine.cpp
+++ Engine/src/Engine.cpp
@@ -38,7 +38,7 @@ void LogSDLVersion(const std::string& message, int major, int minor, int patch)
 
 void LoopCallback(void* arg)
 {
-	static_cast<REC::Minigin*>(arg)->RunOneFrame();
+	static_cast<REC::Engine*>(arg)->RunOneFrame();
 }
 #endif
 
