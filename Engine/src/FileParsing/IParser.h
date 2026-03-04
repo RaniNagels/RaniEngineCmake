cb36043486d776b4bb2e2e8d5ede42ffca624166
RaniNagels <rani.nagels@outlook.be>
2026-03-04 16:34:35 +0100

    make destructor of interface virtual!

--- Engine/src/FileParsing/IParser.h
+++ Engine/src/FileParsing/IParser.h
@@ -8,7 +8,7 @@ namespace REC
 	{
 	public:
 		explicit IParser() = default;
-		~IParser() = default;
+		virtual ~IParser() = default;
 
 		IParser(const IParser&) = delete;
 		IParser(IParser&&) = delete;
