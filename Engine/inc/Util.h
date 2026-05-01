9ad0a4ca385f65eed883fb780f410d951e12e08f
RaniNagels <rani.nagels@outlook.be>
2026-05-01 18:27:06 +0200

    Util.h modification

--- Engine/inc/Util.h
+++ Engine/inc/Util.h
@@ -9,12 +9,12 @@ namespace Util
     concept Enum = std::is_enum_v<T>;
 
     template<Enum E>
-    constexpr auto to_underlying(E e) noexcept
+    constexpr uint8_t to_underlying(E e) noexcept
     {
 #if defined(_WIN32)
         return std::to_underlying(e);
 #else
-        return static_cast<std::underlying_type_t<Enum>>(e);
+        return static_cast<uint8_t>(e);
 #endif
     }
 }
